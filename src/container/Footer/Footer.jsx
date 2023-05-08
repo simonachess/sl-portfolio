e48fc84bd39d729fccacc5081fa7349cf59005ca
simonachess <simonachess@gmail.com>
2023-05-08 16:15:33 +0300

    update error

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -38,7 +38,6 @@ const Footer = () => {
   return (
     <>
       <h2 className="text-5xl font-extrabold text-center uppercase">Take a coffee & chat with me</h2>
-
       <div className="w-[60%] flex justify-evenly items-center flex-wrap mt-16 mx-8 mb-8">
         <div className="min-w-[290px] flex flex-row justify-start items-center my-4 p-4 rounded-xl cursor-pointer bg-[#fef4f5] hover:shadow-xl transition-all duration-300 ease-in-out">
           <img src={images.email} alt="email" className="w-10 h-10 mx-3" />
@@ -91,6 +90,7 @@ const Footer = () => {
           >
             {!loading ? 'Send Message' : 'Sending...'}
           </button>
+          {error && <p>Upss... Something went wrong...</p>}
         </div>
       ) : (
         <div>
